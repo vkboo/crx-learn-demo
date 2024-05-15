1a6d26eced526e2a94a83099a31c315e6a8a616d
bo.zhao <bo.zhao@iglooinsure.com>
2024-05-15 17:51:25 +0800

    feat: conent scripts

---
+++ js/content-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // var div = document.querySelector('#trans-to');
+    console.log('我被执行了！', {
+        'chrome.extension': chrome.extension
+    });
+});
+
+console.log('空闲时执行')
